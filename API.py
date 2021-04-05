ee0f96ab447499d3405eeec12c0ea709287f88ad
Owen <oc289@exeter.ac.uk>
2021-04-05 15:16:54 +0100

    Updated API

--- API.py
+++ API.py
@@ -8,27 +8,19 @@ port = 8080
 names = ["Tom Cruise", "David Balne", "Abigail Anderson", "Joel Miller", "Oscar Isaac", "Sarah Taylor", "Janet Mason", "Catherine Jones", "Alice Evans", "Ella Wright", "Logan Brown"]
 adresses = ["12 Waterloo Road","69 Waterfront Plaza","104 Apex Street","76 Grethound Gardens","32 Trynadmere Court","88 Province Islands","72 District Lane","66 Pointer Close","48 Aztec Drive"," 9000 Penny Lane"]
 post_codes = ["IP11 7QE","RH19 2QP","NE16 5SN","CT21 4HJ","EH14 1HH","DL13 1QX","BS10 6UA","WV13 1DO","TS14 6BG","SG4 9DA"]
-items = ["Apple iPhone 12 Refurbished", "Samsung Galaxy Fold Z", "One Plus Nord", "Apple iPhone 12 New", "Apple Magic Mouse","Leather Wallet","Logitech Wireless keyboard","Google Home","Vinyl Record Player","USB-C to HDMI Cable"]
-pricesGBP = [500, 2000, 389, 800, 99, 20, 30, 50, 150, 6]
 
 class OrderToSend:
-    # class variable: records last id, gets incremented in init method
-    last_id = 0
 
     def __init__(self):
-        item_index = random.randint(0, 10 - 1)
         random_index = random.randint(0, 10 - 1)
         name = names[random_index]
-        
-        self.order_id = self.last_id
+    
         self.name = f'{names[random_index]}'
         self.address = f'{adresses[random_index]}'
         self.post_code = f'{post_codes[random_index]}'
         self.email = f'{name.replace(" ", "")}@gmail.com'
-        self.item = f'{items[item_index]}'
-        self.item_price_gbp = pricesGBP[item_index]
+        self.item_id = random.randint(1, 10)
 
-        OrderToSend.last_id += 1
 
 
 class Server(BaseHTTPRequestHandler):
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Webbay is a moc webserver designed to serve data for the assignemnt ECM2429. It creates a local host server and sends random data with an indexed id on a get request.
 Note: the order_id field does not persist across multiple runs. This means once the webserver has closed the index for the ID will start back at 0
 # API.py
-The API file contains the webserver run this file to start the webserver on localhost.
+The API file contains the webserver run this file to start the webserver on localhost. I reccomend running this in a terminal or having the assignemnt app start this on launch.
 # Consumer.py
 The consumer file contsains an example of how to call a GET request from the web server it then prints the returned value. Make sure the webserver is running before running this.
